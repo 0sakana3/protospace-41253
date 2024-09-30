3093f179e3d65bb4b17ba2c7864a5213eb7cec10
0sakana3 <shakex3.tp3@gmail.com>
2024-09-30 16:19:12 +0900

    ユーザーのアクセス範囲の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show, :update]
-  before_action :move_to_index, only: [:edit, :new ]
-  before_action :authenticate_user!, except: [:index, :show, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:edit, :show, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -54,10 +54,8 @@ class PrototypesController < ApplicationController
   end
 
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 
 end
